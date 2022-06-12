6e95b852b340089f40dcc668bb60454effb0a72b
unknown <arestre@ncsu.edu>
2022-06-12 16:24:57 -0400

    added each hour section

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,11 @@
 console.log ('hello world');
 
+// Date function
 const displayDate = document.getElementById('currentDay')
 
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var mm = String(today.getMonth() + 1).padStart(2, '0'); 
 var yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
