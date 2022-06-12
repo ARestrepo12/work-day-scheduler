c1a1e4b7f0a639284dccd8c511c5f59e8c397e73
unknown <arestre@ncsu.edu>
2022-06-12 15:41:38 -0400

    added date

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+console.log ('hello world');
+
+const displayDate = document.getElementById('currentDay')
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+displayDate.innerHTML = today
