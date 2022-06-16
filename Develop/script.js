d49e59c332d75a1164e2eb3ac504855b90cfd5f6
unknown <arestre@ncsu.edu>
2022-06-16 16:34:08 -0400

    first button saves to localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
-const message = document.getElementById('9');
-var tasks = {};
 
+var tasks = {};
+const save1 =document.getElementById('save1')
+const save2 =document.getElementById('save2')
+const save3 =document.getElementById('save3')
+const save4 =document.getElementById('save4')
+const save5 =document.getElementById('save5')
+const save6 =document.getElementById('save6')
+const save7 =document.getElementById('save7')
+const save8 =document.getElementById('save8')
+const save9 =document.getElementById('save9')
+const message = document.getElementById('9')
+
+
+
+// const message = save1, save2, save3, save4, save5, save6, save7, save8, save9
 
 // Date function
 const displayDate = document.getElementById('currentDay')
@@ -17,14 +30,18 @@ displayDate.innerHTML = today
 //     tasks = JSON.parse(localStorage.getItem("tasks"));
 // };
 
-window.localStorage.setItem("tasks","message.value")
+// window.localStorage.setItem("tasks","message.value")
 
 
 // save button 
-document.querySelectorAll('.save').forEach(item => {
-    item.addEventListener('click', test)
-  })
+// document.querySelectorAll('.save').forEach(item => {
+//     item.addEventListener('click', test)
+//   })
 
-function test () {
-    console.log(message.value)
-}
+// function test () {
+//     console.log(message.value)
+// }
+
+save1.addEventListener('click', function() {
+    localStorage.setItem('task1desc', `${message.value}` )
+  });
